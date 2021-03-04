dbd162a9919243666fc13359b69f4a21067f5567
이원제 <50200484+leewonje418@users.noreply.github.com>
2021-03-04 19:40:21 +0900

    [UPDATE] 모듈 불러오기

--- server/app.js
+++ server/app.js
@@ -1,16 +1,23 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import config from './config'
+import cors from 'cors'
+import bodyParser from 'body-parser';
 
 const app = express();
 const { MONGO_URI } = config;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+app.use(express.json());
 
 mongoose
     .connect(MONGO_URI, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
     })
-    .then(() => console.log("MongoDB connecting Success!!"))
+    .then(() => console.log('MongoDB connecting Success!!'))
     .catch((e) => console.log(e));
 app.get('/');
 
