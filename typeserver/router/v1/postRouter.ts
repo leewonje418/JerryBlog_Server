b199f22f0d7779b044458698b56a50c407969702
이원제 <50200484+leewonje418@users.noreply.github.com>
2021-03-30 20:32:23 +0900

    [UPDATE] 게시글 라우터 추가

---
+++ typeserver/router/v1/postRouter.ts
@@ -0,0 +1,11 @@
+import { Router } from 'express';
+//import PostController from '../../controller/post.controller';
+
+const router: Router = Router();
+
+//const postController = new PostController();
+
+//router.get('/', postController.getPosts);
+//router.post('/', postController.createPost);
+
+export default router;
