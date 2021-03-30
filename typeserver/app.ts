153d99a56b77adcdedae8d8309e352b5d9312db1
이원제 <50200484+leewonje418@users.noreply.github.com>
2021-03-30 20:24:14 +0900

    [UPDATE] app.ts 수정 및 라우터 적용

--- typeserver/app.ts
+++ typeserver/app.ts
@@ -3,15 +3,18 @@ import cors from 'cors';
 import bodyParser from 'body-parser';
 import hpp from 'hpp';
 import helmet from 'helmet';
+import api from './router'
 import path from 'path';
 
 const app = express();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use('/static', express.static(path.join(__dirname, '../public')));
 app.use(cors());
 app.use(hpp());
 app.use(helmet());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use('/static', express.static(path.join(__dirname, '../public')));
+
+app.use('/api', api);
 
 export default app;
