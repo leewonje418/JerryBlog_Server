003deda829eab5c843f58522aa6d1b2f518b6bb3
이원제 <50200484+leewonje418@users.noreply.github.com>
2021-03-30 20:31:59 +0900

    [UPDATE] 인증 라우터 구현

---
+++ typeserver/router/v1/authRouter.ts
@@ -0,0 +1,12 @@
+import { Router } from 'express';
+// import AuthController from '../../controller/auth.controller';
+
+const router: Router = Router();
+
+// const authController = new AuthController();
+
+// router.post('/login', authController.login);
+// router.post('/logout', authController.logout);
+// router.post('/user', authController.user);
+
+export default router;
