fb490a82e720a38019ad470c20a01b26fe0bc03d
이원제 <50200484+leewonje418@users.noreply.github.com>
2021-03-30 20:32:37 +0900

    [UPDATE] 유저 라우터 추가

---
+++ typeserver/router/v1/userRouter.ts
@@ -0,0 +1,11 @@
+import { Router } from 'express';
+//import UserController from '../../controller/user.controller';
+
+const router: Router = Router();
+
+//const userController = new UserController();
+
+//router.get('/', userController.getUsers);
+//router.post('/signup', userController.signUp);
+
+export default router;
