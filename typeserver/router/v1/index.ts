ffc95213a69fc8dddabf68631a01462e975541f8
이원제 <50200484+leewonje418@users.noreply.github.com>
2021-03-30 20:31:42 +0900

    [UPDATE] 메인 라우터

--- typeserver/router/v1/index.ts
+++ typeserver/router/v1/index.ts
@@ -1,4 +1,13 @@
 import { Router } from 'express';
+
+import postRouter from './postRouter';
+import userRouter from './userRouter';
+import authRouter from './authRouter';
+
 const router: Router = Router();
+
+router.use('/post', postRouter);
+router.use('/user', userRouter);
+router.use('/auth', authRouter);
 
 export default router;
