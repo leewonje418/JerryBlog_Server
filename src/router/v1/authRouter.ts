a651bdd9d3c78e0ba38e033375b88dd0c8ba0b4c
이원제 <50200484+leewonje418@users.noreply.github.com>
2021-04-03 22:20:25 +0900

    [UPDATE] 주석추가

--- src/router/v1/authRouter.ts
+++ src/router/v1/authRouter.ts
@@ -5,8 +5,10 @@ const router: Router = Router();
 
 // const authController = new AuthController();
 
+//용도에띠라 이름을 변경할 수 있음
+// router.get('/user', authController.user);
+
 // router.post('/login', authController.login);
 // router.post('/logout', authController.logout);
-// router.post('/user', authController.user);
 
 export default router;
