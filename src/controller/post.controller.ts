4e76844ef73dae82dba4815542272266881c6468
이원제 <50200484+leewonje418@users.noreply.github.com>
2021-04-03 22:18:14 +0900

    [UPDATE] postcontroller 완료

--- src/controller/post.controller.ts
+++ src/controller/post.controller.ts
@@ -1,4 +1,41 @@
 import { Request, Response } from 'express';
+import { Document } from 'mongoose';
+
+import httpError from '../error/httpError';
+
+import PostRequest from '../request/post.request';
+import PostService from '../service/post.service';
 
 export default class PostController {
+    private readonly postService: PostService;
+
+    constructor() {
+        this.postService = new PostService();
+    }
+
+    getPosts = async(req: Request, res: Response) => {
+        const posts: Document<any>[] = await this.postService.get();
+        res.status(200).json({
+            posts
+        })
+    }
+
+    createPost = async(req: Request, res: Response) => {
+        const postRequest = new PostRequest(req.body);
+
+        await postRequest.validate();
+
+        try {
+            const post = this.postService.create(postRequest);
+            if (post === undefined) {
+                throw new httpError(500, '게시글등록 서버 오류');
+            }
+            res.status(200).json({
+                post
+            })
+        } catch (error) {
+            throw new httpError(500, '게시글등록 서버 오류');
+        }
+    }
+
 }
