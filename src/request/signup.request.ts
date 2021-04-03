5a6881ba523e6ca4cff1b393b52b54a1c79757b9
이원제 <50200484+leewonje418@users.noreply.github.com>
2021-04-03 22:20:10 +0900

    [UPDATE] 회원가입 검증 재작

---
+++ src/request/signup.request.ts
@@ -0,0 +1,27 @@
+import { Length, validate } from 'class-validator';
+import HttpError from '../error/httpError';
+
+export default class SignUpRequest {
+  @Length(2, 100)
+  readonly name: string;
+
+  @Length(2, 100)
+  readonly email: string;
+
+  @Length(2, 20)
+  readonly password: string;
+  
+  constructor(body: SignUpRequest) {
+    this.name = body.name;
+    this.email = body.email;
+    this.password = body.password;
+  }
+  
+  async validate(): Promise<void> {
+    const errors = await validate(this);
+  
+    if (errors.length > 0) {
+      throw new HttpError(400, '검증 오류');
+    }
+  }
+}
