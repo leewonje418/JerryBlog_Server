cf1af3329642ff3550d566a11ffdc9a613b1b5a2
이원제 <50200484+leewonje418@users.noreply.github.com>
2021-04-03 22:21:47 +0900

    [UPDATE] userservier 재작(함수 틀 재작)

---
+++ src/service/user.service.ts
@@ -0,0 +1,24 @@
+import mongoose, { Document } from 'mongoose';
+
+import User from '../database/post';
+import HttpError from '../error/httpError';
+import SignUpRequest from '../request/signup.request';
+
+export default class UserService {
+    findAll = async (): Promise<Document<any>[]> => {
+        const users = await User.find();
+        return users;
+    }
+
+    signUp = async (signuprequest : SignUpRequest): Promise<Document<any>> => {
+        const { name, email, password } = signuprequest;
+        try {
+            const newUser = await User.create({
+                name, email, password
+            });
+            return newUser;
+        } catch (error) {
+            throw new HttpError(500, '회원가입 오류');
+        }
+    }
+}
