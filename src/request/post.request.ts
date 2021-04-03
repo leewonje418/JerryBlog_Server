b3966cd1b6f7ad4c29f155240772cd0da14d40f0
이원제 <50200484+leewonje418@users.noreply.github.com>
2021-04-03 22:19:51 +0900

    [UPDATE] 게시글 등록 검증 재작

---
+++ src/request/post.request.ts
@@ -0,0 +1,29 @@
+import { Length, validate } from 'class-validator';
+import HttpError from '../error/httpError';
+
+export default class PostRequest {
+  @Length(1, 50)
+  readonly title: string;
+
+  @Length(1, 500)
+  readonly content: string;
+
+  readonly fileUrl: string;
+
+  readonly creator: string;
+
+  constructor(body: PostRequest) {
+    this.title = body.title;
+    this.content = body.content;
+    this.fileUrl = body.fileUrl;
+    this.creator = body.creator;
+  }
+
+  async validate(): Promise<void> {
+    const errors = await validate(this);
+
+    if (errors.length > 0) {
+      throw new HttpError(400, '재목 또는 내용의 글자를 초과했습니다.');
+    }
+  }
+}
