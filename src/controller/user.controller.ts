209b42c4496f89b2af30fcb304ce5313bd1767e6
이원제 <50200484+leewonje418@users.noreply.github.com>
2021-04-04 21:54:20 +0900

    [UPDATE] usercontroller 재작

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -1,4 +1,39 @@
 import { Request, Response } from 'express';
+import { Document } from 'mongoose';
+import httpError from '../error/httpError';
+import SignUpRequest from '../request/signup.request';
+import UserService from '../service/user.service';
 
 export default class UserController {
+    private readonly userService: UserService;
+
+    constructor() {
+        this.userService = new UserService();
+    }
+
+    getUsers = async(req: Request, res: Response) => {
+        const users: Document<any>[] = await this.userService.getUsers();
+        res.status(200).json({
+            users
+        })
+    }
+
+    signUp = async(req: Request, res: Response) => {
+        const signupRequest = new SignUpRequest(req.body);
+
+        await signupRequest.validate();
+
+        try {
+            const newUser = this.userService.signUp(signupRequest);
+            if (newUser === undefined) {
+                throw new httpError(500, '게시글등록 서버 오류');
+            }
+            res.status(200).json({
+                message: '회원가입 성공',
+                data : newUser
+            })
+        } catch (error) {
+            throw new httpError(500, '게시글등록 서버 오류');
+        }
+    }
 }
