5499a9994fe1a702d75cce3521c123f13518973c
이원제 <50200484+leewonje418@users.noreply.github.com>
2021-04-05 13:52:03 +0900

    [UPDATE] 쉼표 삭제

--- src/lib/handler/httpSuccessHandler.ts
+++ src/lib/handler/httpSuccessHandler.ts
@@ -3,7 +3,7 @@ import { Response } from 'express';
 export default (res: Response, status: number, message: string, data?: any) => {
 	res.status(status).json({
 		message: message,
-		data: data && data,
+		data: data && data
 	});
 
     return;
