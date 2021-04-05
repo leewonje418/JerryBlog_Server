8ede7724e3dece48acf1a59806fc64eaa1d06564
이원제 <50200484+leewonje418@users.noreply.github.com>
2021-04-05 13:53:46 +0900

    [UPDATE] 게시글 dto import 및 사용, 성공 헨들러 사용

--- src/controller/post.controller.ts
+++ src/controller/post.controller.ts
@@ -1,10 +1,10 @@
-import { Request, Response } from 'express';
+import { Request, response, Response } from 'express';
 import { Document } from 'mongoose';
 
 import SuccessHandler from '../lib/handler/httpSuccessHandler';
 import ErrorHandler from '../lib/handler/httpErrorHandler';
 
-import PostRequest from '../request/post.request';
+import PostDTO from '../dto/post.dto';
 import PostService from '../service/post.service';
 
 export default class PostController {
@@ -17,24 +17,19 @@ export default class PostController {
     getPosts = async(req: Request, res: Response) => {
         try {
             const posts: Document<any>[] = await this.postService.getPosts();
-            res.status(200).json({
-                posts
-            })
+            SuccessHandler(response, 200, '게시글 전채 불러오기 성공', posts);
         } catch (err) {
             ErrorHandler(res, err);
         }
     }
 
     create = async(req: Request, res: Response) => {
-        const postRequest = new PostRequest(req.body);
-
+        const postRequest = new PostDTO(req.body);
         await postRequest.validate();
 
         try {
             const post = this.postService.create(postRequest);
-            res.status(200).json({
-                post
-            })
+            SuccessHandler(response, 200, '게시글 게시 성공', post);
         } catch (err) {
             ErrorHandler(res, err);
         }
