3a0e6ab9190af375063e5047e899d87875053b78
이원제 <50200484+leewonje418@users.noreply.github.com>
2021-04-05 13:52:59 +0900

    [UPDATE] 로그인헨들러, 로그인 dto import 및 사용

--- src/controller/auth.controller.ts
+++ src/controller/auth.controller.ts
@@ -1,9 +1,10 @@
-import { Request, Response } from 'express';
+import { Request, response, Response } from 'express';
 
 import SuccessHandler from '../lib/handler/httpSuccessHandler';
+import LoginSuccessHandler from '../lib/handler/httpLoginSuccessHandler';
 import ErrorHandler from '../lib/handler/httpErrorHandler';
 
-import LoginRequest from '../request/login.request';
+import LoginDTO from '../dto/login.dto';
 import AuthService from '../service/auth.service';
 
 export default class AuthController {
@@ -16,33 +17,25 @@ export default class AuthController {
     // 로그인 헨들러 구현해야됨
     login = async (req: Request, res:Response) => {
         try {
-            const loginRequest = new LoginRequest(req.body);
+            const loginRequest = new LoginDTO(req.body);
             await loginRequest.validate();
 
             const token = await this.authService.login(loginRequest);
-
-            res.status(200).json({
-                message: '로그인 성공',
-                data: {
-                  'x-access-token': token,
-                },
-            });
+            LoginSuccessHandler(response, 200, '로그인 성공', token);
         } catch (err) {
             ErrorHandler(res, err);
         }
     }
 
     logout = async (res: Response) => {
-        res.status(200).json({
-            message: '로그아웃 성공'
-        })
+        SuccessHandler(response, 200, '로그아웃 성공');
     }
 
     //용도에띠라 이름을 변경할 수 있음 자료형도 바뀔수 있음 
     user = async (req: any, res: Response) => {
         try {
             const user = await this.authService.user(req.user.id);
-            res.status(200).json(user);
+            SuccessHandler(response, 200, '유저 전체조회 성공', user);
         } catch (err) {
             ErrorHandler(res, err);
         }
