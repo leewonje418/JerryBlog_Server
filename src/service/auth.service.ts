63b9b9752750326e7572a669e5027c5832cc5ae6
이원제 <50200484+leewonje418@users.noreply.github.com>
2021-04-06 22:04:09 +0900

    [UPDATE] 암호화 수정, 호스트 인증함수 추가

--- src/service/auth.service.ts
+++ src/service/auth.service.ts
@@ -1,5 +1,4 @@
-import * as bcrypt from "bcryptjs";
-import mongoose, { Document } from 'mongoose';
+import { Document } from 'mongoose';
 
 import Bcrypt from '../lib/bcrypt/bcrypt'
 import User from '../database/user';
@@ -9,10 +8,10 @@ import LoginDTO from '../dto/login.dto'
 
 export default class AuthService {
     login = async (loginRequest: LoginDTO): Promise<string> => {
-        let { email, password } = loginRequest;
+        const { email, pw } = loginRequest;
 
         const bcrypt = new Bcrypt();
-        password = bcrypt.hashPassword(password);
+        const password = bcrypt.hashPassword(pw.toString());
         
         User.findOne({email, password}).then(async (user) => {
             if(user === undefined) {
@@ -23,10 +22,17 @@ export default class AuthService {
         return token;
     }
 
-    //용도에띠라 이름을 변경할 수 있음
     user = async (id: number): Promise<Document<any>> => {
         const user = await User.findById(id).select("-password");
         if(!user) {
+            throw new HttpError(401, '유저가 존재하지 않습니다.');
+        }
+        return user;
+    }
+
+    host = async (id: number): Promise<Document<any>> => {
+        const user = await User.findOne({ id, role: 'MainHost' }).select('-password');
+        if(user === null) {
             throw new HttpError(401, '유저가 존재하지 않습니다.');
         }
         return user;
