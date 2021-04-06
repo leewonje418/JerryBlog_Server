21ae628776cabf0bfa62e32546f44111c0797938
이원제 <50200484+leewonje418@users.noreply.github.com>
2021-04-06 22:03:43 +0900

    [UPDATE] 호스트 인증 미들웨어 추가

--- src/router/v1/postRouter.ts
+++ src/router/v1/postRouter.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 import PostController from '../../controller/post.controller';
+import { authHost } from '../../lib/middleware/auth.middleware';
 
 const router: Router = Router();
 
 const postController = new PostController();
 
 router.get('/', postController.getPosts);
-router.post('/', postController.create);
+router.post('/', authHost , postController.create);
 
 export default router;
