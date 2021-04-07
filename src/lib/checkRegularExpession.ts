c9ccf04d6afdbed3370b44c483cbb8e7b91cb600
이원제 <50200484+leewonje418@users.noreply.github.com>
2021-04-07 12:05:17 +0900

    [UPDATE] 메세지 변경

--- src/lib/checkRegularExpession.ts
+++ src/lib/checkRegularExpession.ts
@@ -4,10 +4,10 @@ export const checkRegularExpession = async (email: string, pw: string) => {
     const emailExperssion: RegExp = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
     const pwExperssion: RegExp = /^.*(?=^.{8,15}$)(?=.*\d)(?=.*[a-zA-Z])(?=.*[!@#$%^&+=]).*$/;
     if(!emailExperssion.test(email)) {
-        throw new HttpError(400, '이메일이 조건을 만족하지 않습니다.');
+        throw new HttpError(400, '이메일이 형식을 만족하지 않습니다.');                                         
     }
     else if(!pwExperssion.test(pw)) {
-        throw new HttpError(400, '비밀번호가 조건을 만족하지 않습니다.');
+        throw new HttpError(400, '비밀번호가 형식을 만족하지 않습니다.');
     }
     return;
 }
