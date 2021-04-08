074bf6e6e4186f0ba15549ad4205a8e72355a216
이원제 <50200484+leewonje418@users.noreply.github.com>
2021-04-08 21:35:59 +0900

    [UPDATE] typeorm init

---
+++ src/orm.ts
@@ -0,0 +1,33 @@
+import {
+    Connection,
+    createConnection,
+    ConnectionOptions,
+  } from 'typeorm';
+  import logger from './lib/logger';
+  import entities from './entity';
+  import { MYSQL } from './config/config';
+  
+  export const getConnection = async (): Promise<Connection> => {
+  
+    const connectionOptions: ConnectionOptions = {
+      type: 'mysql',
+      database: MYSQL.DATABASE,
+      synchronize: MYSQL.SYNC,
+      logging: false,
+      host: MYSQL.HOST,
+      port: MYSQL.PORT,
+      username: MYSQL.USERNAME,
+      password: MYSQL.PASSWORD,
+      charset: 'utf8',
+      entities,
+    };
+  
+    try {
+      const connection = createConnection(connectionOptions);
+      logger.success('[DB] connected');
+      return connection;
+    } catch (err) {
+      logger.error('[DB] Connection Error', err.message);
+      throw err;
+    }
+  };
