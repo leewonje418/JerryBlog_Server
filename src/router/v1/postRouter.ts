7a1ddc33d5018c17d18830cc163728b0fb4a1dc6
Lee\Je <50200484+leewonje418@users.noreply.github.com>
2021-04-18 20:43:57 +0900

    [RENAME] auth -> authHost

--- src/router/v1/postRouter.ts
+++ src/router/v1/postRouter.ts
@@ -1,12 +1,12 @@
 import { Router } from 'express';
 import PostController from '../../controller/post.controller';
-import { auth } from '../../lib/middleware/auth.middleware';
+import { authHost } from '../../lib/middleware/auth.middleware';
 
 const router: Router = Router();
 
 const postController: PostController = new PostController();
 
 router.get('/', postController.getPosts);
-router.post('/', auth, postController.create);
+router.post('/', authHost, postController.create);
 
 export default router;
