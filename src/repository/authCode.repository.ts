6726f4a2b36f961da825bd794e5c51da1123325e
Lee\Je <50200484+leewonje418@users.noreply.github.com>
2021-04-26 13:45:03 +0900

    [UPDATE] 이메일 코드 삭제 및 이메일 코드 비교 후 인증 기능 추가

---
+++ src/repository/authCode.repository.ts
@@ -0,0 +1,19 @@
+import { DeleteQueryBuilder, EntityRepository, Repository } from 'typeorm';
+import AuthCode from '../entity/authCode';
+
+@EntityRepository(AuthCode)
+export default class AuthCodeRepository extends Repository<AuthCode>{
+    deleteCode = async (email: string): Promise<DeleteQueryBuilder<AuthCode>> => {
+        return this.createQueryBuilder('authcode')
+            .delete()
+            .where(`email = :email`, { email })
+    }
+    findByEmailAndCode = async (email: string, code: string): Promise<AuthCode | undefined> => {
+        return this.findOne({
+            where: {
+                email,
+                code,
+            },
+        });
+    }
+}
