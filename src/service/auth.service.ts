ec2ac5f9871ede601c9db19c325daabeb0edc176
Lee\Je <50200484+leewonje418@users.noreply.github.com>
2021-04-26 13:46:07 +0900

    [UPDATE] emailcode 삭제, email 인증, 이메일 중복 체크 추가

--- src/service/auth.service.ts
+++ src/service/auth.service.ts
@@ -5,6 +5,8 @@ import { createToken } from '../lib/token'
 import LoginDTO from '../dto/login.dto'
 import UserRepository from '../repository/user.repository';
 import { getCustomRepository } from 'typeorm';
+import AuthCode from 'src/entity/authCode';
+import AuthCodeRepository from 'src/repository/authCode.repository';
 
 export default class AuthService {
     login = async (loginRequest: LoginDTO): Promise<string | undefined> => {
@@ -45,6 +47,15 @@ export default class AuthService {
         return user;
     }
 
+    existEmail = async (email: string): Promise<User | undefined> => {
+        const userRepository: UserRepository = getCustomRepository(UserRepository);
+        const user: User | undefined = await userRepository.findUser(email);
+        if(user !== undefined) {
+            throw new HttpError(401, '중복된 이메일 입니다.');
+        }
+        return user;
+    }
+
     getHost = async (email: string): Promise<User> => {
         const userRepository: UserRepository = getCustomRepository(UserRepository);
         const host: User | undefined = await userRepository.findByEmailAndRole(email);
@@ -52,5 +63,40 @@ export default class AuthService {
             throw new HttpError(401, '호스트가 존재하지 않습니다.');
         }
         return host;
+    }
+
+    emailCheck = async (email: string): Promise<User | undefined> => {
+        const userRepository: UserRepository = getCustomRepository(UserRepository);
+        const host: User | undefined = await userRepository.findOne(email);
+        if(host !== undefined) {
+            throw new HttpError(401, '이미 존재하는 이메일 입니다.');
+        }
+        return host;
+    }
+
+    createEmailCode = async(email: string, code: string): Promise<AuthCode> => {
+        const authCodeRepository: AuthCodeRepository = getCustomRepository(AuthCodeRepository);
+
+        const authCode: AuthCode = new AuthCode();
+        authCode.email = email;
+        authCode.code = code;
+
+        const newCode = await authCodeRepository.save(authCode);
+
+        return newCode;
+    }
+
+    deleteEmailCode = async(email: string) => {
+        const authCodeRepository: AuthCodeRepository = getCustomRepository(AuthCodeRepository);
+        await authCodeRepository.deleteCode(email);
+    }
+
+    checkEmailCode = async (email: string, code: string): Promise<AuthCode | undefined> => {
+        const authCodeRepository: AuthCodeRepository = getCustomRepository(AuthCodeRepository);
+        const emailCode: AuthCode | undefined = await authCodeRepository.findByEmailAndCode(email, code);
+        if(emailCode === undefined) {
+            throw new HttpError(401, '이메일 코드가 잘못되었습니다.');
+        }
+        return emailCode; 
     }
 }
