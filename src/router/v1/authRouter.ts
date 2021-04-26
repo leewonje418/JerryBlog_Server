418f326f2a5fded59dbc1db843fe23b5290904d3
Lee\Je <50200484+leewonje418@users.noreply.github.com>
2021-04-26 13:45:33 +0900

    [UPDATE] 라우터 추가(이메일 인증, 이메일 코드 전송)

--- src/router/v1/authRouter.ts
+++ src/router/v1/authRouter.ts
@@ -8,5 +8,7 @@ const authController: AuthController = new AuthController();
 router.post('/login', authController.login);
 router.post('/logout', authController.logout);
 router.post('/emailDuplicate', authController.emailDuplicate);
+router.post('/sendEmail', authController.sendEmail);
+router.post('/checkEmail', authController.checkEmailCode);
 
 export default router;
