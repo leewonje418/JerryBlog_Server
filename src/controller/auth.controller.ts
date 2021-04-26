7d6d0ccab37e84dbad0a06c2f13299279d90761b
Lee\Je <50200484+leewonje418@users.noreply.github.com>
2021-04-26 13:43:38 +0900

    [UPDATE] 이메일 비교, 이메일 인증코드 생성 및 전송, 이메일 인증코드 비교 기능 추가

--- src/controller/auth.controller.ts
+++ src/controller/auth.controller.ts
@@ -5,6 +5,8 @@ import ErrorHandler from '../lib/handler/httpErrorHandler';
 
 import LoginDTO from '../dto/login.dto';
 import AuthService from '../service/auth.service';
+import emailCode from 'src/lib/email/emailCode';
+import sendEmail from 'src/lib/email/sendEmail';
 
 export default class AuthController {
     private readonly authService: AuthService;
@@ -27,5 +29,44 @@ export default class AuthController {
 
     logout = async (req: Request, res: Response) => {
         successHandler(res, 200, '로그아웃 성공');
+    }
+
+    emailDuplicate = async (req: Request, res:Response) => {
+        try {
+            const { email } = req.body;
+            const findEmail = await this.authService.emailCheck(email);
+            if(findEmail === undefined) {
+                successHandler(res, 200, '이메일 중복 확인 성공');
+            }
+        } catch (err) {
+            ErrorHandler(res, err);
+        }
+    }
+
+    sendEmail = async (req: Request, res: Response)=> {
+        try {
+            const { email } = req.body;
+
+            await this.authService.deleteEmailCode(email);
+
+            const code: string = emailCode();
+            await this.authService.createEmailCode(email, code);
+
+            await sendEmail(email, code);
+
+            successHandler(res, 200, '이메일 코드를 발송하였습니다.');
+        } catch (err) {
+            ErrorHandler(res, err);
+        }
+    }
+    
+    checkEmailCode = async (req: Request, res: Response)=> {
+        try {
+            const { email, code } = req.body;
+            await this.authService.checkEmailCode(email, code);
+            successHandler(res, 200, '이메일 인증 성공');
+        } catch (err) {
+            ErrorHandler(res, err);
+        }
     }
 }
