86b4a6ed2abbf399b54ef370716c9bc473992054
Lee\Je <50200484+leewonje418@users.noreply.github.com>
2021-04-28 22:15:44 +0900

    [UPDATE] 이메일 인증 모듈 사용

--- src/service/user.service.ts
+++ src/service/user.service.ts
@@ -6,28 +6,37 @@ import { checkRegularExpession } from '../lib/checkRegularExpession';
 import UserRepository from '../repository/user.repository';
 import { getCustomRepository } from 'typeorm';
 import Role from '../enum/Role';
+import AuthCodeRepository from 'src/repository/authCode.repository';
+import AuthCode from 'src/entity/authCode';
 
 export default class UserService {
     getUsers = async (): Promise<User[]> => {
-        const userRepository = getCustomRepository(UserRepository);
-        const users = await userRepository.find();
+        const userRepository: UserRepository = getCustomRepository(UserRepository);
+        const users: User[] = await userRepository.find();
         return users;
     }
 
     signUp = async (signUpRequest : SignUpDTO): Promise<User> => {
-        const userRepository = getCustomRepository(UserRepository);
+        const userRepository: UserRepository = getCustomRepository(UserRepository);
+        const authCodeRepository: AuthCodeRepository = getCustomRepository(AuthCodeRepository);
+
         const { name, email, pw } = signUpRequest;
         const { USER } = Role;
 
-        const checkprofile = await userRepository.findOne({ email });
+        const checkprofile: User | undefined = await userRepository.findOne({ email });
         if(checkprofile) {
             throw new HttpError(400, '이메일 중복');
         }
 
         await checkRegularExpession(email, pw);
 
-        const bcrypt = new Bcrypt();
-        const password = bcrypt.hashPassword(pw);
+        const checkEmailCode: AuthCode | undefined = await authCodeRepository.findByEmailAndCheck(email);
+        if(checkEmailCode === undefined) {
+            throw new HttpError(400, '이메일 인증을 완료해주세요.');
+        }
+
+        const bcrypt: Bcrypt = new Bcrypt();
+        const password: string = bcrypt.hashPassword(pw);
 
         const user: User = new User();
 		user.email = email;
@@ -35,7 +44,7 @@ export default class UserService {
         user.password = password;
         user.role = USER;
 
-        const newUser = await userRepository.save(user)
+        const newUser: User = await userRepository.save(user)
 
         return newUser;
     }
