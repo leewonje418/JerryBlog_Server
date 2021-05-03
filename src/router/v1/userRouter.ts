cd8caa5e4693fd940105311bf9958b454eb51d52
이원제 <50200484+leewonje418@users.noreply.github.com>
2021-05-03 22:23:48 +0900

    [UPDATE] user update, delete url 추가

--- src/router/v1/userRouter.ts
+++ src/router/v1/userRouter.ts
@@ -1,11 +1,15 @@
 import { Router } from 'express';
+import { authUser } from 'src/lib/middleware/auth.middleware';
 import UserController from '../../controller/user.controller';
 
 const router: Router = Router();
 
 const userController: UserController = new UserController();
 
-router.get('/', userController.getUsers);
+router.get('/getUsers', userController.getUsers);
+// router.get('/getUsers', userController.getUsers);
 router.post('/signup', userController.signUp);
+router.put('/updateName', authUser, userController.updateName);
+router.delete('/delete', authUser, userController.delete);
 
 export default router;
