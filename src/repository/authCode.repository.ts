42e7ef74248d18b629cf33eee818281219dbeddf
이원제 <50200484+leewonje418@users.noreply.github.com>
2021-05-03 22:22:28 +0900

    [DELETE] deleteCode 함수 삭제

--- src/repository/authCode.repository.ts
+++ src/repository/authCode.repository.ts
@@ -3,11 +3,6 @@ import AuthCode from '../entity/authCode';
 
 @EntityRepository(AuthCode)
 export default class AuthCodeRepository extends Repository<AuthCode>{
-    deleteCode = async (email: string): Promise<DeleteQueryBuilder<AuthCode>> => {
-        return this.createQueryBuilder('authcode')
-            .delete()
-            .where(`email = :email`, { email })
-    }
     findByEmailAndCode = async (email: string, code: string): Promise<AuthCode | undefined> => {
         return this.findOne({
             where: {
