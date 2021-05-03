4b6b5b2700bbb6a44d652341140fab79aab79d39
이원제 <50200484+leewonje418@users.noreply.github.com>
2021-05-03 22:25:03 +0900

    [UPDATE] 개별 불러오기, update 추가

--- src/service/post.service.ts
+++ src/service/post.service.ts
@@ -10,6 +10,12 @@ export default class PostService {
         return posts;
     }
 
+    getPost = async (): Promise<Post | undefined> => {
+        const postRepository: PostRepository = getCustomRepository(PostRepository);
+        const post: Post | undefined = await postRepository.findOne();
+        return post;
+    }
+
     create = async (hostEmail: string, postRequest: PostDTO): Promise<Post> => {
         const postRepository: PostRepository = getCustomRepository(PostRepository);
         const { title, content, image } = postRequest;
@@ -23,5 +29,25 @@ export default class PostService {
         const newPost: Post = await postRepository.save(post);
         
         return newPost;
+    }
+
+    update = async (id: number, postRequest: PostDTO): Promise<Post> => {
+        const postRepository: PostRepository = getCustomRepository(PostRepository);
+        const { title, content, image } = postRequest;
+
+        const post: Post = new Post();
+        post.idx = id;
+		post.title = title;
+        post.content = content;
+        post.image = image;
+
+        const newPost: Post = await postRepository.save(post);
+        
+        return newPost;
+    }
+
+    delete = async (id: number) => {
+        const postRepository: PostRepository = getCustomRepository(PostRepository);
+        await postRepository.delete(id);
     }
 }
