bf3940708420dc34acf24f32b362fa35938c48ed
이원제 <50200484+leewonje418@users.noreply.github.com>
2021-05-05 20:50:37 +0900

    [UPDARE] 개별 정보 조회 재작

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -6,18 +6,31 @@ import ErrorHandler from '../lib/handler/httpErrorHandler';
 import SignUpDTO from '../dto/signup.dto';
 import UserService from '../service/user.service';
 import User from '../entity/user';
+import AuthService from 'src/service/auth.service';
 
 export default class UserController {
     private readonly userService: UserService;
+    private readonly authService: AuthService;
 
     constructor() {
         this.userService = new UserService();
+        this.authService = new AuthService();
     }
 
     getUsers = async(req: Request, res: Response) => {
         try {
             const users: User[] = await this.userService.getUsers();
             successHandler(res, 200, '유저 전채 불러오기 성공', users);
+        } catch (err) {
+            ErrorHandler(res, err);
+        }
+    }
+
+    getUser = async(req: Request, res: Response) => {
+        try {
+            const { userEmail } = req;
+            const users: User = await this.authService.getUser(userEmail);
+            successHandler(res, 200, ' 내정보 불러오기 성공', users);
         } catch (err) {
             ErrorHandler(res, err);
         }
@@ -39,7 +52,7 @@ export default class UserController {
         try {
             const { userEmail } = req;
             const { name } = req.body;
-            
+
             await this.userService.updateName(userEmail, name);
 
             successHandler(res, 200, '이름수정 성공');
