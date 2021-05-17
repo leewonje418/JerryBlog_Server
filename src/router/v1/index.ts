685f26782a061d7278c99d2328e3a7598abd4f08
이원제 <50200484+leewonje418@users.noreply.github.com>
2021-05-17 20:18:51 +0900

    [UPDATE] search, comment 라우터 추가

--- src/router/v1/index.ts
+++ src/router/v1/index.ts
@@ -3,11 +3,15 @@ import { Router } from 'express';
 import postRouter from './postRouter';
 import userRouter from './userRouter';
 import authRouter from './authRouter';
+import searchRouter from './searchRouter';
+import commentRouter from './commentRouter';
 
 const router: Router = Router();
 
 router.use('/post', postRouter);
 router.use('/user', userRouter);
 router.use('/auth', authRouter);
+router.use('/search', searchRouter);
+router.use('/comment', commentRouter);
 
 export default router;
