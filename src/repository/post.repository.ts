523687e8685c6e53836fbfe14b6874b002b48c7b
이원제 <50200484+leewonje418@users.noreply.github.com>
2021-05-17 20:18:28 +0900

    [UPDATE] findByTitle 함수 추가

--- src/repository/post.repository.ts
+++ src/repository/post.repository.ts
@@ -3,4 +3,9 @@ import Post from '../entity/post';
 
 @EntityRepository(Post)
 export default class PostRepository extends Repository<Post> {
+    findByTitle = async (keyword: string): Promise<Post[]> => {
+        return this.createQueryBuilder('post')
+            .where('title ILIKE :searchKeyword', {searchKeyword: keyword})
+            .getMany();
+    }
 }
