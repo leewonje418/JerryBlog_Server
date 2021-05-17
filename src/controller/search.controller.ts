e9c1f74ecf48b824852bf470af29c3e8c3624a9e
이원제 <50200484+leewonje418@users.noreply.github.com>
2021-05-17 20:17:59 +0900

    [UPDATE] 검색기능 컨트롤러 재작

---
+++ src/controller/search.controller.ts
@@ -0,0 +1,24 @@
+import { Request, Response } from 'express';
+
+import { successHandler } from '../lib/handler/httpSuccessHandler';
+import ErrorHandler from '../lib/handler/httpErrorHandler';
+import SearchService from 'src/service/search.service';
+import Post from 'src/entity/post';
+
+export default class PostController {
+    private readonly searchService: SearchService;
+
+    constructor() {
+        this.searchService = new SearchService();
+    }
+
+    search = async(req: Request, res: Response) => {
+        try {
+            const { keyword } = req.body;
+            const searchPosts: Post[] | undefined = await this.searchService.search(keyword);
+            successHandler(res, 200, '검색된 게시글 전채 불러오기 성공', searchPosts);
+        } catch (err) {
+            ErrorHandler(res, err);
+        }
+    }
+}
